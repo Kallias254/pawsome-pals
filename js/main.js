69496238a2f2cc68a10296cb01adc7f72629136f
miseda <misedajoseph@gmail.com>
2023-09-20 01:05:59 +0300

    pawsome pals

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Wait for the image to load
+window.addEventListener('load', function () {
+    var img = document.getElementById('dog-image');
+
+    // Set explicit width and height attributes for the image
+    img.width = 800; // Adjust to your preferred width
+    img.height = 600; // Adjust to your preferred height
+});
+
+document.getElementById('new-dog').addEventListener('click', function() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => {
+            let dogContainer = document.getElementById('dog-container');
+            // clear the previous image
+            dogContainer.innerHTML = '';
+            let dogImage = document.createElement('img');
+            dogImage.src = data.message;
+            dogContainer.appendChild(dogImage);
+        })
+        .catch(error => console.error('Error:', error));
+});
+
