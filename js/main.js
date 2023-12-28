25ff2821fb79f877c3db7abdca5ddf41f3491187
K254 <misedajoseph@gmail.com>
2023-12-28 19:14:27 +0300

    update api call

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // Wait for the image to load
 window.addEventListener('load', function () {
-    callApi();
     var img = document.getElementById('dog-image');
 
     // Set explicit width and height attributes for the image
     img.width = 800; // Adjust to your preferred width
     img.height = 600; // Adjust to your preferred height
+    //call api
+    callApi();
+
 });
 
 document.getElementById('new-dog').addEventListener('click', callApi);
